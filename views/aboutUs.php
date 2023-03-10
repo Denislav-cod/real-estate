57b97b0d4325f554492f8e8775851f47923b611c
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2023-03-11 01:34:58 +0200

    Header and footer replaced with html elements

--- views/aboutUs.php
+++ views/aboutUs.php
@@ -1,35 +1,45 @@
-<!-- Header -->
-<?php include "header.php" ?>
-<!-- Navigtion -->
-<?php include "navigation.php" ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-if(!isset($_COOKIE['logAndReg'])){
-    header('Location: http://localhost/php/Php-project/Project/view/login.php');
-} 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-<div class="container  mt-5">
-    <div class="row justify-content-center">
-        <div class="col-lg-7 mt-3 p-4 me-3">
-            <h3>Кои сме ние</h3>
-            <p>Намираме се във Варна, България</p>
+<body>
+    <!-- Navigtion -->
+    <?php include "navigation.php" ?>
+
+    <?php
+    if (!isset($_COOKIE['logAndReg'])) {
+        header('Location: http://localhost/php/Php-project/Project/view/login.php');
+    }
+    ?>
+
+    <div class="container  mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-7 mt-3 p-4 me-3">
+                <h3>Кои сме ние</h3>
+                <p>Намираме се във Варна, България</p>
+            </div>
+            <div class="col-lg-4  mt-3 p-4 me-3 ">
+                <h3>Инструменти, които сме използвали</h3>
+                <p><code style="font-size: 18px;">PHP, MYSQL, HTML, BOOTSTRAP, CSS</code></p>
+            </div>
         </div>
-        <div class="col-lg-4  mt-3 p-4 me-3 ">
-            <h3>Инструменти, които сме използвали</h3>
-            <p><code style="font-size: 18px;">PHP, MYSQL, HTML, BOOTSTRAP, CSS</code></p>
+        <div class="row justify-content-center">
+            <div class="col-lg-2 mt-3 p-4 me-3">
+                <h3>Контакти</h3>
+                <br>
+                <p>Телефон: 089089089</p>
+                <p>Имейл: test.gmail.com</p>
+            </div>
         </div>
     </div>
-    <div class="row justify-content-center">
-        <div class="col-lg-2 mt-3 p-4 me-3">
-            <h3>Контакти</h3>
-            <br>
-            <p>Телефон: 089089089</p>
-            <p>Имейл: test.gmail.com</p>
-        </div>
     </div>
-</div>
-</div>
 
-<!-- Footer -->
-<?php include "./footer.php" ?>
+</body>
+
+</html>
