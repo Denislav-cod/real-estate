6f46dec604f94aebb41ce334cc209a1b49649ff6
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2023-03-11 15:36:39 +0200

    Header and footer replaced with html elements

--- views/login.php
+++ views/login.php
@@ -1,29 +1,37 @@
-<!-- Header -->
-<?php include_once("header.php") ?>
-<!-- Navigtion -->
-<?php include_once("navigation.php") ?>
-
 <?php include_once("../functions/functions.php") ?>
 
-<?php
-if (isset($_POST['login'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $message = login($email, $password);
-    if($message){
-        echo
-                "<div class='row justify-content-center text-center m-5'>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Navigtion -->
+    <?php include_once("navigation.php") ?>
+
+    <?php
+    if (isset($_POST['login'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $message = login($email, $password);
+        if ($message) {
+            echo
+            "<div class='row justify-content-center text-center m-5'>
                 <div class='col-md-3'>
                     <div class='alert alert-danger text'>
                         $message
                     </div>
                 </div>
             </div>";
+        }
     }
-}
-?>
+    ?>
 
-<body>
     <form action="#" method="POST" class="m-5">
         <div class="row justify-content-center mb-3">
             <div class="col-sm-3">
@@ -43,6 +51,7 @@ if (isset($_POST['login'])) {
             </div>
         </div>
     </form>
+
 </body>
 
-<?php include_once("./footer.php") ?>
+</html>
